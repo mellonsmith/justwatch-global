b87cf7edfdbc31a60f4c1d2fe8297e76b70339df
mellonsmith <mellon385champi@gmail.com>
2023-08-02 18:24:53 +0200

    Fixed error that occured if Movie was found ,but didnt have any Streaming Provider

--- globalsearch.py
+++ globalsearch.py
@@ -6,20 +6,21 @@ def get_streaming_providers(title, mycountry):
     just_watch = JustWatch(country=mycountry)
     results = just_watch.search_for_item(query=title)
     streaming_providers = []
-    if (len(results['items']) != 0):
-        for provider in results['items'][0]['offers']:
-            if provider['monetization_type'] == 'flatrate':     # Only include providers that offer a flatrate subscription
-                provider_dict = [
-                    provider['package_short_name'],
-                    provider['urls']['standard_web']
-                    ]
-                streaming_providers.append(provider_dict)
+    if (len(results['items']) != 0):                                
+        if 'offers' in results['items'][0]:                         # Prevents error if Movie was found but has no streaming options
+            for provider in results['items'][0]['offers']:
+                if provider['monetization_type'] == 'flatrate':     # Only include providers that offer a flatrate subscription
+                    provider_dict = [
+                        provider['package_short_name'],
+                        provider['urls']['standard_web']
+                        ]
+                    streaming_providers.append(provider_dict)
     return streaming_providers 
 
 def main():
     title = input("Enter the title of the movie or TV show: ")
     print("Searching for streaming providers...")
-    found = False
+    found = False                                                   # Prevents error if Movie is not existing
     for country in searchcountrys:
         streaming_providers = get_streaming_providers(title, country)
         if len(streaming_providers) != 0:
